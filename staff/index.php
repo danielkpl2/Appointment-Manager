3ad6a9440dcc2d6ce47b28dabc24eec21eff3185
Daniel Kasprowicz <danielkpl2@gmail.com>
2015-04-01 07:02:50 +0100

    - removed commented out code from staff/index.php - changed the input fields' text-align property to center on staff page - changed the colours for '#today', '.available', '.clicked', and '.timeslots'

--- staff/index.php
+++ staff/index.php
@@ -87,141 +87,29 @@ if(!login_check($mysqli)){
             <h4>Available appointment times for Helen Purchase</h4>
             <div id="timeslots">
 
-                <!--<table class='table'><tr><th style="width: 30px"></th><th>Date</th><th>Start</th><th>End</th><th>Duration</th><th>Purpose</th><th>Student</th><th>Note</th></tr></table>-->
                 <table class='table'><tr><th style="width: 30px"></th><th>Date</th><th>Start</th><th>End</th><th>Duration</th><th>Purpose</th><th>Student</th></tr></table>
                 <table class="table slots"></table>
                 <form name="form_staff">
-                <table class="table insert"><tr><td style="width: 30px"><button  name="submit" class='btn btn-xs' type="submit"><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></button></td>
+                <table class="table insert">
+
+                        <tr><td style="width: 30px"><button  name="submit" class='btn btn-xs' type="submit"><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></button></td>
                         <td><input type="text" class="form-control" name="date"/></td>
-                    <td><input type="text" class="form-control" name="start"/></td><td ><input type="text" class="form-control" name="end"/></td>
+                        <td><input type="text" class="form-control" name="start"/></td><td ><input type="text" class="form-control" name="end"/></td>
                         <td></td>
                         <td></td>
-                        <td></td><td></td>
-                    </tr>
+                        <td></td>
+                        </tr>
+
                 </table>
-
                 </form>
-                <!--class="btn btn-default "-->
-
-
-
-                <!--
-                List of timeslots goes here
-                <table class='table'><tr><th>Date</th><th>Start</th><th>End</th><th>Duration</th><th>Student</th><th>Purpose</th><th>Note</th></tr>
-                <table class="table form"></table>
-                -->
-
 
             </div>
-            <!--
-            <div id="form-staff">
 
-                <form class="form-horizontal" name="form-staff">
-
-
-
-                    <div class="form-group show">
-                        <label class="col-md-4 control-label" for="name">Name</label>
-                        <div class="col-md-6">
-                            <input id="name" name="name" placeholder="" class="form-control input-md" required type="text">
-
-                        </div>
-                    </div>
-
-
-                    <div class="form-group show">
-                        <label class="col-md-4 control-label" for="email">Email address</label>
-                        <div class="col-md-6">
-                            <input id="email" name="email" placeholder="" class="form-control input-md" required type="text">
-
-                        </div>
-                    </div>
-
-
-                    <div class="form-group show">
-                        <label class="col-md-4 control-label" for="GUID">GUID</label>
-                        <div class="col-md-6">
-                            <input id="guid" name="guid" placeholder="" class="form-control input-md" type="text">
-
-                        </div>
-                    </div>
-
-                    <div class="form-group hide">
-                        <label class="col-md-4 control-label" for="GUID/email">GUID/Email</label>
-                        <div class="col-md-6">
-                            <input id="guid/email" name="guid/email" placeholder="" class="form-control input-md" type="text">
-
-                        </div>
-                    </div>
-
-
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="password">Password</label>
-                        <div class="col-md-6">
-                            <input id="password" name="password" placeholder="" class="form-control input-md" required type="password">
-
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="purpose">Purpose</label>
-                        <div class="col-md-6">
-                            <select id="purpose" name="purpose" class="form-control">
-                                <option value="1">General</option>
-                                <option value="2">Advisor</option>
-                                <option value="3">Studies</option>
-                            </select>
-                        </div>
-                    </div>
-
-
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="note">Note</label>
-                        <div class="col-md-6">
-                            <textarea class="form-control" id="note" name="note"></textarea>
-                        </div>
-                    </div>
-
-
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="submit"></label>
-                        <div class="col-md-6">
-                            <button id="submit" name="submit" class="btn btn-default" type="submit">Submit</button>
-                        </div>
-                    </div>
-
-                </form>
-
-            </div> -->
             <div class="response"></div>
 
         </div>
         <div class="col-md-3">
-            <?php
-            //$_POST["staff"] = "1";
-            include ("../calendar.php");
-
-            /*
-            $url = '../calendar.php';
-            $data = array('staff' => '1');
-
-            // use key 'http' even if you send the request to https://...
-            $options = array(
-                'http' => array(
-                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                    'method'  => 'POST',
-                    'content' => http_build_query($data),
-                ),
-            );
-            $context  = stream_context_create($options);
-            $result = file_get_contents($url, false, $context);
-
-            echo var_dump($result);
-            */
-
-            ?>
+            <?php include ("../calendar.php"); ?>
 
         </div>
     </div>
